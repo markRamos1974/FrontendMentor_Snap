2d46f2e3f731e00fdc110fc7a487ed4ea23da072
cosmos <markadreianramos41@gmail.com>
2022-12-25 20:16:33 +0800

    Snap

---
+++ script.js
@@ -0,0 +1,30 @@
+const hamburgerMenu = document.querySelector(".hamburger-menu")
+const navigationContainer = document.querySelector(".nvaigation-container")
+const headerNavigation = document.querySelector(".header-navigation")
+const overlay = document.querySelector(".overlay")
+
+const dropDownMenuButton = document.querySelectorAll(".nav-list-item")
+const dropDownMenu = document.querySelectorAll(".list")
+const arrows = document.querySelectorAll(".arrow")
+
+
+for(let i = 0; i < 2; i++){
+    dropDownMenuButton[i].addEventListener("click", ()=>{
+        dropDownMenu[i].classList.toggle("show-menu")
+        arrows[i].classList.toggle("rotate-arrow")
+    })
+}
+
+let isMenuClosed = true
+hamburgerMenu.addEventListener("click", ()=> {
+
+    const hambugerMenuClass = hamburgerMenu.classList
+    const headerNavigationClass = headerNavigation.classList
+    const overlayClass = overlay.classList
+
+    hambugerMenuClass.toggle("open-hamburger-menu")
+    headerNavigationClass.toggle("open-header-navigation")
+    overlayClass.toggle("show-overlay")
+})
+
+
